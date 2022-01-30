6c9bc12ddb4ff7f10737aa66dd5445d634523850
esq0604 <esq0604@gmail.com>
2022-01-31 04:19:26 +0900

    조건문

---
+++ Chapter5-2/조건문if.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	cin >> x;
+
+	if (x > 10) 
+		cout << x << " is greater than 10" << endl;
+	
+	else
+		cout << x << " not greater than 10" << endl;
+	return 0;
+}
